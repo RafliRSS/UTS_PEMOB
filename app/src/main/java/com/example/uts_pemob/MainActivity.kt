55b8d217fd05b09434914148c76801b025ab5b70
RafliRSS <abangjunedi9@gmail.com>
2024-11-04 18:50:14 +0700

    Finish

--- app/src/main/java/com/example/uts_pemob/MainActivity.kt
+++ app/src/main/java/com/example/uts_pemob/MainActivity.kt
@@ -1,16 +1,35 @@
-package com.example.uts_pemob
+package com.example.belajar
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.RelativeLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.uts_pemob.DetailActivity
+import com.example.uts_pemob.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val detail = findViewById<RelativeLayout>(R.id.rl)
+        detail.setOnClickListener {
+            val intent = Intent(this, DetailActivity::class.java)
+            startActivity(intent)
+        }
+
+        val gotopage = findViewById<RelativeLayout>(R.id.rl2)
+        gotopage.setOnClickListener {
+            val url = "https://id.portal-pokemon.com/play/pokedex/0001"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
